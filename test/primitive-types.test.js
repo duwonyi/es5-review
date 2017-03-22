000d9fca5791625c1b5f58159dc7042f71fda5f4
Doowon Lee <duwonyi@gmail.com>
2017-03-22 16:01:28 -0400

    add test for primitive types

--- test/primitive-types.test.js
+++ test/primitive-types.test.js
@@ -1,8 +1,20 @@
 var assert = require('chai').assert;
 var foo = null;
+var bar;
+
 
 describe('Primitive Types', () => {
-  it('typeof null is object', () => {
+  it('Typeof null is object', () => {
     assert.equal(typeof foo, 'object');
+  });
+
+  it('A undefined variable is undefined', () => {
+    assert.equal(typeof bar, 'undefined');
+  });
+
+  it('There are five primitive types in JavaScript', () => {
+    assert.isNumber(4, '4 is number');
+    assert.isBoolean(true, 'true is boolean');
+    assert.isString('str', 'str is string');
   });
 });
